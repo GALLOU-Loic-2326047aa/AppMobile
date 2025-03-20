3c39224bbabf6a71aa1b520204baa558a0658658
gallo <loic.gallou@etu.univ-amu.fr>
2025-03-20 17:25:37 +0100

    premier

--- src/component/App.js
+++ src/component/App.js
@@ -4,13 +4,7 @@ import BDGA1 from '../jsonBD/BDGA21.json'
 
 function App() {
 
-  const [nombre,setNombre] = useState(0);
-
   const [taches, setTaches] = useState([]);
-
-  const ajout = () => {
-    setNombre(nombre+1);
-  };
 
   useEffect(() =>{
     setTaches(BDGA1.taches);
